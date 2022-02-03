483bab0374ab36b2a00d74c8efeb0c048ad1d440
jackthomas00 <40306951+jackthomas00@users.noreply.github.com>
2022-02-02 21:51:15 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -10,13 +10,13 @@ public class SkillDemoTest {
   @Test
   public void enumerateTest() {
     Integer counter = 10;
-    assertEquals(13, (int) SkillDemo.enumerate(counter));
+    assertEquals(11, (int) SkillDemo.enumerate(counter));
   }
 
   @Test
   public void getValue() throws IOException {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-    assertEquals(12, (int) SkillDemo.getNumberFromList(numbers, 4));
+    assertEquals(5, (int) SkillDemo.getNumberFromList(numbers, 4));
   }
 
 }
